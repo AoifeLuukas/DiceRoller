9c99dd08b1682a2a8ec069d9d60883e9cd068ad6
AoifeLuukas <aoife.luukas@gmail.com>
2022-04-12 21:06:55 -0400

    moved the D6 view from the main activity to a fragment and added the D20 fragment

--- app/src/main/java/com/example/diceroller/D20DiceFragment.kt
+++ app/src/main/java/com/example/diceroller/D20DiceFragment.kt
@@ -6,10 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.example.diceroller.databinding.FragmentD20DiceBinding
 import com.example.diceroller.databinding.FragmentD6DiceBinding
 
-class D6DiceFragment : Fragment() {
-    private var _binding: FragmentD6DiceBinding? = null
+class D20DiceFragment : Fragment() {
+
+    private var _binding: FragmentD20DiceBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -17,11 +19,11 @@ class D6DiceFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentD6DiceBinding.inflate(inflater, container, false)
+        _binding = FragmentD20DiceBinding.inflate(inflater, container, false)
         val view = binding.root
 
         binding.rollButton.setOnClickListener {
-            rollD6Dice()
+            rollD20Dice()
         }
 
         return view
@@ -32,23 +34,23 @@ class D6DiceFragment : Fragment() {
         _binding = null
     }
 
-    private fun rollD6Dice() {
+    private fun rollD20Dice() {
         val diceText: TextView = binding.resultText
-        val diceRange = 1..6
+        val diceRange = 1..20
         val diceResult = diceRange.random()
         val diceTextResult = "You got a $diceResult"
         diceText.text = diceTextResult
 
-        val diceImageResult = when (diceResult) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-
-        binding.diceImage.setImageResource(diceImageResult)
+//        val diceImageResult = when (diceResult) {
+//            1 -> R.drawable.dice_1
+//            2 -> R.drawable.dice_2
+//            3 -> R.drawable.dice_3
+//            4 -> R.drawable.dice_4
+//            5 -> R.drawable.dice_5
+//            else -> R.drawable.dice_6
+//        }
+//
+//        binding.diceImage.setImageResource(diceImageResult)
 
     }
 }
